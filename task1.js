beaf6063069b75913e21467bd73c6d5af99047c8
sayed725 <ssayed72533@gmail.com>
2024-09-30 14:19:04 +0600

    my first commit

---
+++ task1.js
@@ -0,0 +1,21 @@
+const arrow = (num1, num2, num3) => num1 * num2 * num3;
+
+console.log(arrow(1, 2, 3))
+
+
+const ok = `I am a web developer. I love to code. I love to eat biryani`
+console.log(ok)
+
+
+const result = (a, b = 0) => a + b;
+
+console.log(result(10))
+
+
+const friends = ['alamin', 'milton', 'nirob', 'shawon', 'samiul']
+
+
+const songkha = friends.filter(p=>p.length%2===0)
+
+
+console.log(songkha)
---
+++ task2.js
@@ -0,0 +1,10 @@
+const arr = [10, 20, 30, 40, 50]
+
+
+
+
+const brr = [5,15,25,30,40]
+
+const marge = [...arr,...brr]
+const maxx = Math.max(...marge)
+console.log(maxx)
